9779205f8c186e96e1c8fb8b7ef9f4799ca92157
Anirvan BASU <Anirvan.BASU@INRIA.fr>
2016-09-19 10:27:06 +0200

    Adapted Ceph related files for image jessie-x64-big + modified apt.pp

---
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/install_ceph/apt.pp
@@ -0,0 +1,40 @@
+class env::nfs::install_ceph::apt (
+  $version = 'firefly'
+) {
+
+  # bug ref. 7225 - changed commands to get ceph packages from apt.grid5000.fr
+  file {
+    '/etc/apt/sources.list.d/ceph.list':
+      ensure  => file,
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      content => "deb http://apt.grid5000.fr/ceph5k/jessie/${version}"
+  }
+
+  # bug ref. 7225 - changed commands to install ceph from correct packages
+  package {
+    "ceph":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+    "chrony":
+      ensure => installed;
+    "ceph-common":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+    "python-ceph":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+    "librbd1":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+    "libcephfs1":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+    "librados2":
+      ensure => '0.80.10-2~bpo8+1',
+      require => File["/etc/apt/sources.list.d/ceph.list"];
+  }
+
+}
+
