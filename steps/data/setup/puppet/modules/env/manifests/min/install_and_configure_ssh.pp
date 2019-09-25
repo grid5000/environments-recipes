54f8cef1ec2fdbfa70e94cb2f87e4c21382cde4b
PHILIPPOT Sebastien <sebastien.philippot@irisa.fr>
2019-09-25 11:09:10 +0200

    [env::min::ssh] remove UsePrivilegeSeparation no, deprecated

--- steps/data/setup/puppet/modules/env/manifests/min/install_and_configure_ssh.pp
+++ steps/data/setup/puppet/modules/env/manifests/min/install_and_configure_ssh.pp
@@ -46,7 +46,6 @@ class env::min::install_and_configure_ssh {
         'set /files/etc/ssh/sshd_config/HostKey[1] /etc/ssh/ssh_host_rsa_key',
         'set /files/etc/ssh/sshd_config/HostKey[2] /etc/ssh/ssh_host_ecdsa_key',
         'set /files/etc/ssh/sshd_config/HostKey[3] /etc/ssh/ssh_host_ed25519_key',
-        'set /files/etc/ssh/sshd_config/UsePrivilegeSeparation no',
         'set /files/etc/ssh/sshd_config/PermitRootLogin without-password',
         'set /files/etc/ssh/sshd_config/PermitUserEnvironment yes',
         'set /files/etc/ssh/sshd_config/MaxStartups 500'
