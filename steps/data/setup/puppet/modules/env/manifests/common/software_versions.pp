ece79e6565b9b9f13c77e5c8229de9e149e12579
Dimitri DELABROYE <dimitri.delabroye@inria.fr>
2021-02-04 16:34:56 +0100

    [puppet/common/software_versions] set multiples versions for ganglia

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -6,10 +6,22 @@ class env::common::software_versions {
    $tgz_g5k = '2.0.15'
    $g5k_checks = '0.10.6'
    $sudo_g5k = '1.9'
-   $ganglia_monitor = '3.6.0-7.1'
    $libguestfs_backport_arm64 = '1:1.40.2-7~bpog5k10+1'
    $libguestfs_backport_ppc64el = '1:1.40.2-7~bpog5k10+1'
    $lmod = '6.6-0.3g5k1'
    $datacenter_gpu_manager = '1:1.7.2'
    $g5k_jupyterlab = '0.4'
+
+  case $lsbdistcodename {
+    'buster': {
+      $ganglia_monitor = '3.6.0-7.1'
+    }
+    'bullseye': {
+      $ganglia_monitor = '3.7.2-4'
+    }
+    default: {
+      $ganglia_monitor = '3.6.0-7.1'
+    }
+
+  }
 }
