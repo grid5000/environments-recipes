0432319a4c41faea7f92d00a007da8113b960081
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-02-10 11:16:20 +0100

    [debian11] Fix ganglia-monitor service duplicate declaration

--- steps/data/setup/puppet/modules/env/manifests/base/install_and_disable_ganglia.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/install_and_disable_ganglia.pp
@@ -45,22 +45,9 @@ class env::base::install_and_disable_ganglia ($enable = false){
     }
   }
 
-  # Debian jessie suffers a bug that make puppet unable to correctly detect if some services are enabled or not (https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=760616 or #bug=751638 )
-  # Because of this bug, service ganglia-monitor can't be made "disabled" with the standard method.
-  if "${::lsbdistcodename}" == "jessie" and "${::operatingsystem}" == "Debian" {
-    unless $enable {
-      exec {
-        'disable ganglia-monitor':
-          command => '/bin/systemctl disable ganglia-monitor',
-          require => Package['ganglia-monitor'];
-      }
-    }
-  }
-  else {
-    service {
-      'ganglia-monitor':
-        enable  => $enable,
-        require => Package['ganglia-monitor'];
-    }
+  service {
+    'ganglia-monitor':
+      enable  => $enable,
+      require => Package['ganglia-monitor'];
   }
 }
--- steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
@@ -3,36 +3,37 @@ class env::big::configure_nvidia_gpu::ganglia () {
   case $operatingsystem {
     'Debian','Ubuntu': {
 
-      env::common::g5kpackages {
-        'ganglia-monitor-nvidia':
-          packages => 'ganglia-monitor-python-nvidia',
-          ensure => installed;
-      }
-
-      Package['ganglia-monitor'] -> Package['ganglia-monitor-python-nvidia']
-
       case "${::lsbdistcodename}" {
         # ganglia 3.7.X
         'bullseye' : {
-          file{
-            '/etc/ganglia/conf.d/modpython-nvidia.conf':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/modpython-nvidia-3.7.conf",
-              require => Package['ganglia-monitor-python-nvidia'];
-            '/etc/systemd/system/ganglia-monitor.service':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/ganglia-monitor-3.7-override.service",
-              notify  => Service['ganglia-monitor'];
-           }
+          # Unsupported ganglia-monitor-python-nvidia package for now
+          #file{
+          #  '/etc/ganglia/conf.d/modpython-nvidia.conf':
+          #    ensure  => file,
+          #    owner   => root,
+          #    group   => root,
+          #    mode    => '0644',
+          #    source  => "puppet:///modules/env/big/nvidia/modpython-nvidia-3.7.conf",
+          #    require => Package['ganglia-monitor-python-nvidia'];
+          #  '/etc/systemd/system/ganglia-monitor.service':
+          #    ensure  => file,
+          #    owner   => root,
+          #    group   => root,
+          #    mode    => '0644',
+          #    source  => "puppet:///modules/env/big/nvidia/ganglia-monitor-3.7-override.service",
+          #    notify  => Service['ganglia-monitor'];
+          # }
         }
         # ganglia 3.6.X
         default : {
+          env::common::g5kpackages {
+            'ganglia-monitor-nvidia':
+              packages => 'ganglia-monitor-python-nvidia',
+              ensure => installed;
+          }
+
+          Package['ganglia-monitor'] -> Package['ganglia-monitor-python-nvidia']
+
           file{
             '/etc/ganglia/conf.d/modpython-nvidia.conf':
               ensure  => file,
@@ -51,14 +52,14 @@ class env::big::configure_nvidia_gpu::ganglia () {
            }
         }
       }
-      service {
-        'ganglia-monitor':
-          enable    => true;
+      exec {
+        'enable ganglia-monitor':
+          command => '/bin/systemctl enable ganglia-monitor',
+          require => Package['ganglia-monitor'];
       }
     }
     default: {
       err "${operatingsystem} not supported."
     }
   }
-
 }
