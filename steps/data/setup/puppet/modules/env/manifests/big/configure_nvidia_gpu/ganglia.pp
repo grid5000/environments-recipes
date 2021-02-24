bfc9f9d5633193bfec820c61a5915678b94def30
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-02-24 14:33:01 +0100

    Revert "[debian11] ganglia-monitor 3.7 changes"
    This reverts commit 504c794224135017533014ca021c985847f25811.

--- steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
@@ -11,49 +11,26 @@ class env::big::configure_nvidia_gpu::ganglia () {
 
       Package['ganglia-monitor'] -> Package['ganglia-monitor-python-nvidia']
 
-      case "${::lsbdistcodename}" {
-        # ganglia 3.7.X
-        'bullseye' : {
-          file{
-            '/etc/ganglia/conf.d/modpython-nvidia.conf':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/modpython-nvidia-3.7.conf",
-              require => Package['ganglia-monitor-python-nvidia'];
-            '/etc/systemd/system/ganglia-monitor.service':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/ganglia-monitor-3.7-override.service",
-              notify  => Service['ganglia-monitor'];
-           }
-        }
-        # ganglia 3.6.X
-        default : {
-          file{
-            '/etc/ganglia/conf.d/modpython-nvidia.conf':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/modpython-nvidia.conf",
-              require => Package['ganglia-monitor-python-nvidia'];
-            '/etc/systemd/system/ganglia-monitor.service':
-              ensure  => file,
-              owner   => root,
-              group   => root,
-              mode    => '0644',
-              source  => "puppet:///modules/env/big/nvidia/ganglia-monitor.service",
-              notify  => Service['ganglia-monitor'];
-           }
-        }
+      file{
+        '/etc/ganglia/conf.d/modpython-nvidia.conf':
+          ensure  => file,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          source  => "puppet:///modules/env/big/nvidia/modpython-nvidia.conf",
+          require => Package['ganglia-monitor-python-nvidia'];
+        '/etc/systemd/system/ganglia-monitor.service':
+          ensure  => file,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          source  => "puppet:///modules/env/big/nvidia/ganglia-monitor.service";
       }
-      service {
-        'ganglia-monitor':
-          enable    => true;
+      # FIXME ne devrait-on pas utiliser une ressource Service plutot ici ?
+      exec {
+       'Enable ganglia on startup':
+         command => "systemctl enable ganglia-monitor",
+         path    => ['/bin','/usr/bin','/sbin'];
       }
     }
     default: {
