e8fb9314827957b24ff24025b0cfd5785539cc63
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-02-24 14:35:47 +0100

    Revert "[puppet/common/software_versions] set multiples versions for ganglia"
    This reverts commit ece79e6565b9b9f13c77e5c8229de9e149e12579.

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -6,6 +6,7 @@ class env::common::software_versions {
    $tgz_g5k = '2.0.15'
    $g5k_checks = '0.10.6'
    $sudo_g5k = '1.9'
+   $ganglia_monitor = '3.6.0-7.1'
    $libguestfs_backport_arm64 = '1:1.40.2-7~bpog5k10+1'
    $libguestfs_backport_ppc64el = '1:1.40.2-7~bpog5k10+1'
    $lmod = '6.6-0.3g5k1'
