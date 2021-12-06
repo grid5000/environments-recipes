9762019d554d6cc5dadf35320f2acfe2427b69b9
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-12-06 09:06:31 +0100

    [nfs][modules] Add debian11 modules by default on debian11

--- steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
@@ -6,7 +6,7 @@ class env::nfs::configure_module_path () {
       case "${::lsbdistcodename}" {
         "bullseye" : {
           # Bug 13590
-          $modulespath = "/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n"
+          $modulespath = "/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n/grid5000/spack/share/spack/modules/linux-debian11-x86_64\n"
         }
         default: {
           $modulespath = "/grid5000/spack/share/spack/modules/linux-debian9-x86_64\n/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n"
