9658ba0207696e85efaccf7070bf112cca970637
Nicolas Perrin <nicolas.perrin@inria.fr>
2022-04-13 14:29:08 +0200

    [software] Remove tgz-g5k pinning

--- steps/data/setup/puppet/modules/env/manifests/min/install_tgz_g5k.pp
+++ steps/data/setup/puppet/modules/env/manifests/min/install_tgz_g5k.pp
@@ -6,7 +6,7 @@ class env::min::install_tgz_g5k {
 
       env::common::g5kpackages {
         'tgz-g5k':
-          ensure => $::env::common::software_versions::tgz_g5k;
+          ensure => installed;
       }
     }
     default: {
