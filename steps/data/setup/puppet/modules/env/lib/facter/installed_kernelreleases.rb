97d6c963f36fbb165b2eb3783272314f434e8502
Nicolas Perrin <nicolas.perrin@inria.fr>
2026-01-14 13:31:01 +0100

    [debian13][min] Add Puppet recipes

--- steps/data/setup/puppet/modules/env/lib/facter/installed_kernelreleases.rb
+++ steps/data/setup/puppet/modules/env/lib/facter/installed_kernelreleases.rb
@@ -3,10 +3,10 @@ Facter.add(:installed_kernelreleases) do
     kernels = Dir.glob('/boot/{vmlinuz,vmlinux}-*')
 
     kernels.sort_by! do |k|
-      m = /^\/boot\/vmlinu[zx]-(\d+)\.(\d+)\.(\d+)(_|-)(\d+).*$/.match(k)
+      m = /^\/boot\/vmlinu[zx]-(\d+)\.(\d+)\.(\d+)(_|-|\+deb)(\d+).*$/.match(k)
       [m[1].to_i, m[2].to_i, m[3].to_i, m[5].to_i]
     end
 
-    kernels.map { |k| k.gsub(/\/boot\/vmlinu[zx]-(\d+\.\d+\.\d+(_|-)\d+.*)/, '\1') }
+    kernels.map { |k| k.gsub(/\/boot\/vmlinu[zx]-(\d+\.\d+\.\d+(_|-|\+deb)\d+.*)/, '\1') }
   end
 end
