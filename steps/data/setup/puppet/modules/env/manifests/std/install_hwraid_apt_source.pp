4bada220ce205d6aebc518c9173f6e105cf64ef5
Nicolas Perrin <nicolas.perrin@inria.fr>
2026-02-12 13:44:59 +0100

    [debian13][std] Retrieve megacli from hwraid local repository mirror

--- steps/data/setup/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
@@ -3,22 +3,8 @@ class env::std::install_hwraid_apt_source {
   include apt
 
   case "${::lsbdistcodename}" {
-    "trixie" : {
-      # test with bullseye repository (bug #17875)
-      apt::source { 'hwraid.le-vert.net':
-        key      => {
-          'id'      => '0073C11919A641464163F7116005210E23B3D3B4',
-          'content' => template('env/std/hwraid/hwraid.le-vert.net.key.erb'),
-        },
-        comment  => 'Repo for megacli package',
-        location => 'http://hwraid.le-vert.net/debian',
-        release  => "bullseye",
-        repos    => 'main',
-          include  => {
-            'deb' => true,
-            'src' => false
-        }
-      }
+    'trixie' : {
+    # repository no nore used (bug #17875)
     }
     "buster", "bullseye" : {
       apt::source { 'hwraid.le-vert.net':
--- steps/data/setup/puppet/modules/env/manifests/std/install_megacli.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/install_megacli.pp
@@ -1,11 +1,30 @@
 class env::std::install_megacli {
 
-  require env::std::install_hwraid_apt_source
-
-  package {
-    'megacli':
-      ensure => installed,
-      require  => [Apt::Source['hwraid.le-vert.net'], Exec['apt_update']]
+  case "${::lsbdistcodename}" {
+    'trixie' : {
+      # pick and install package using local mirrored bullseye repository (bug #17875)
+      exec {
+        'retrieve_megacli':
+          command   => "/usr/bin/wget -q http://packages.grid5000.fr/deb/hwraid/pool-bullseye/megacli/megacli_8.07.14-3+Debian.11.bullseye_amd64.deb -O /tmp/megacli.deb",
+          creates   => "/tmp/megacli.deb";
+        'retrieve_libncurses5':
+          command   => "/usr/bin/wget -q http://ftp.fr.debian.org/debian/pool/main/n/ncurses/libncurses5_6.2+20201114-2+deb11u2_amd64.deb -O /tmp/libncurses5.deb",
+          creates   => "/tmp/libncurses5.deb";
+        'retrieve_/tmp/libtinfo5':
+          command   => "/usr/bin/wget -q http://ftp.fr.debian.org/debian/pool/main/n/ncurses/libtinfo5_6.2+20201114-2+deb11u2_amd64.deb -O /tmp/libtinfo5.deb",
+          creates   => "/tmp/libtinfo5.deb";
+        'install_megacli':
+          command     => "/usr/bin/dpkg -i /tmp/megacli.deb /tmp/libncurses5.deb /tmp/libtinfo5.deb",
+          user        => root;
+      }
+    }
+    default : {
+      require env::std::install_hwraid_apt_source
+      package {
+        'megacli':
+          ensure => installed,
+          require  => [Apt::Source['hwraid.le-vert.net'], Exec['apt_update']]
+      }
+    }
   }
-
 }
