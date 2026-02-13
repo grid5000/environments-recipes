f5b054471a925da3b4b23dfcc3bc83c4789715e2
PARISOT Clement <clement.parisot@inria.fr>
2026-02-13 16:00:03 +0100

    fix rocm key location on trixie

--- steps/data/setup/puppet/modules/env/manifests/big/configure_amd_gpu.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_amd_gpu.pp
@@ -8,22 +8,45 @@ class env::big::configure_amd_gpu () {
     'bookworm' => 'jammy',
     'trixie'   => 'noble'
   }
+  case $::lsbdistcodename {
+    'buster' : {
+      apt::source {
+        'repo.radeon.com':
+          comment      => 'Repo for AMD ROCM packages',
+          location     => $rocm_source_url,
+          release      => 'ubuntu',
+          repos        => 'main',
+          architecture => 'amd64',
+          key          => {
+            'id'     => '1A693C5C',
+            'source' => 'https://repo.radeon.com/rocm/rocm.gpg.key',
+          },
+          include      => {
+            'deb' => true,
+            'src' => false
+          },
+          notify       => Exec['apt_update'],
+      }
+    }
 
-  exec {
-    'retrieve rocm key':
-      command => "/usr/bin/wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | gpg --dearmor | sudo tee /usr/share/keyrings/amdgpu.gpg > /dev/null";
-  }
+    default : {
+      exec {
+        'retrieve rocm key':
+          command => "/usr/bin/wget https://repo.radeon.com/rocm/rocm.gpg.key -O - | gpg --dearmor | tee /etc/apt/trusted.gpg.d/rocm.gpg > /dev/null";
+      }
 
-  apt::source {
-    'repo.radeon.com-amdgpu':
-      comment      => 'Repo for AMD AMDGPU packages',
-      location     => $amdgpu_source_url,
-      release      => $amdgpu_releases[$::lsbdistcodename],
-      repos        => 'main',
-      architecture => 'amd64',
-      require      => Exec['retrieve rocm key'],
-      notify       => Exec['apt_update'],
-      include  => { 'deb' => true, 'src' => false }
+      apt::source {
+        'repo.radeon.com-amdgpu':
+          comment      => 'Repo for AMD AMDGPU packages',
+          location     => $amdgpu_source_url,
+          release      => $amdgpu_releases[$::lsbdistcodename],
+          repos        => 'main',
+          architecture => 'amd64',
+          require      => Exec['retrieve rocm key'],
+          notify       => Exec['apt_update'],
+          include  => { 'deb' => true, 'src' => false }
+      }
+    }
   }
 
   package {
