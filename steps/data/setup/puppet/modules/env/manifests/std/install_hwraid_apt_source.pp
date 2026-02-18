67104cf0ccb299d32b5486fb6dd07d0fa3c618e9
Nicolas Perrin <nicolas.perrin@inria.fr>
2026-02-18 16:28:10 +0100

    [debian][std] Udpate hwraid repository key and add trixie

--- steps/data/setup/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
@@ -3,27 +3,26 @@ class env::std::install_hwraid_apt_source {
   include apt
 
   case "${::lsbdistcodename}" {
-    "trixie" : {
-      # test with bullseye repository (bug #17875)
-      apt::source { 'hwraid.le-vert.net':
-        key      => {
-          'id'      => '0073C11919A641464163F7116005210E23B3D3B4',
-          'content' => template('env/std/hwraid/hwraid.le-vert.net.key.erb'),
-        },
-        comment  => 'Repo for megacli package',
-        location => 'http://hwraid.le-vert.net/debian',
-        release  => "bullseye",
-        repos    => 'main',
-          include  => {
-            'deb' => true,
-            'src' => false
-        }
+    'trixie' : {
+      # apt-key is no more supported
+      file {
+        "/usr/share/keyrings/hwraid.gpg":
+          ensure   => file,
+          owner    => root,
+          group    => root,
+          mode     => '0644',
+          content => template('env/std/hwraid/hwraid.le-vert.net.key.erb');
+        "/etc/apt/sources.list.d/hwraid.list":
+          ensure  => present,
+          content => "deb [signed-by=/usr/share/keyrings/hwraid.gpg] http://hwraid.le-vert.net/debian ${::lsbdistcodename} main",
+          notify  => Class['apt::update'],
+          require => File["/usr/share/keyrings/hwraid.gpg"],
       }
     }
-    "buster", "bullseye" : {
+    'buster', 'bullseye' : {
       apt::source { 'hwraid.le-vert.net':
         key      => {
-          'id'      => '0073C11919A641464163F7116005210E23B3D3B4',
+          'id'      => '9B241597ACC8C086A363535E43676E103A9A6F7C',
           'content' => template('env/std/hwraid/hwraid.le-vert.net.key.erb'),
         },
         comment  => 'Repo for megacli package',
