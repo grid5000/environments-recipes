5c8471bb1ef582db5f5b87f8713413188f410a13
Nicolas Perrin <nicolas.perrin@inria.fr>
2026-02-18 16:28:10 +0100

    [debian][std] Udpate hwraid repository key and add trixie

--- steps/data/setup/puppet/modules/env/manifests/std/install_megacli.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/install_megacli.pp
@@ -2,10 +2,21 @@ class env::std::install_megacli {
 
   require env::std::install_hwraid_apt_source
 
-  package {
-    'megacli':
-      ensure => installed,
-      require  => [Apt::Source['hwraid.le-vert.net'], Exec['apt_update']]
+  case "${::lsbdistcodename}" {
+    'trixie' : {
+      package {
+        'megacli':
+          ensure => installed,
+          require  => [File['/etc/apt/sources.list.d/hwraid.list'], Exec['apt_update']]
+      }
+    }
+    'buster', 'bullseye' : {
+      package {
+        'megacli':
+          ensure => installed,
+          require  => [Apt::Source['hwraid.le-vert.net'], Exec['apt_update']]
+      }
+    }
   }
 
 }
