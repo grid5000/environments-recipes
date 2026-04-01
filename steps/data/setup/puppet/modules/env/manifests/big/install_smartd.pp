de54ebf28e39c3d2f785f584edd49cb6cdbb1081
Nicolas Perrin <nicolas.perrin@inria.fr>
2026-04-01 10:42:55 +0200

    [debian13][big] fix smartd smartmontools service failed when no device to monitor (Bug #18116)

--- steps/data/setup/puppet/modules/env/manifests/big/install_smartd.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/install_smartd.pp
@@ -15,7 +15,7 @@ class env::big::install_smartd {
       require => File['/etc/systemd/system/smartd.service.d/'];
   }
 
-  file_line { 'smard.conf':
+  file_line { 'smartd.conf':
     ensure  => present,
     require => Package['smartmontools'],
     path    => '/etc/smartd.conf',
@@ -23,15 +23,25 @@ class env::big::install_smartd {
     match   => '^DEVICESCAN .*';
   }
 
-  # bug 15290: since bookworm, smartmontools fails if there is nothing to monitor, with exit code 17.
-  # only for debian12: "-q never" lets smartd continue to run, waiting to load a configuration file listing valid devices.
-  # for debian13 with v7.4, "-q nodev0" should work like "nodev" with exit code 0.
-  if $::lsbdistcodename == 'bookworm' {
-    file_line { 'smartmontools_config':
-      ensure  => present,
-      require => Package['smartmontools'],
-      path    => '/etc/default/smartmontools',
-      line    => 'smartd_opts="--quit=never"',
+  # Since Bookworm, smartmontools fails if there is nothing to monitor
+  case "${::lsbdistcodename}" {
+    # Bug 18116 - smartmontools v7.4, "-q nodev0" : the exit status is 0 if there are no devices to monitor.
+    'trixie': {
+      file_line { 'smartmontools_config':
+        ensure  => present,
+        require => Package['smartmontools'],
+        path    => '/etc/default/smartmontools',
+        line    => 'smartd_opts="--quit=nodev0"',
+      }
+    }
+    # Bug 15290 - smartmontools v7.3, "-q never" lets smartd continue to run, waiting to load a configuration file listing valid devices.
+    'bookworm': {
+      file_line { 'smartmontools_config':
+        ensure  => present,
+        require => Package['smartmontools'],
+        path    => '/etc/default/smartmontools',
+        line    => 'smartd_opts="--quit=never"',
+      }
     }
   }
 }
