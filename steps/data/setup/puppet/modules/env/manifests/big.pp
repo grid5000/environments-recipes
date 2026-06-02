389aec36a44696cf7b385ed297ba515e1f13fc7b
Philippe Virouleau <philippe.virouleau@inria.fr>
2026-06-02 11:19:08 +0200

    fix arm64 build for debian11/12

--- steps/data/setup/puppet/modules/env/manifests/big.pp
+++ steps/data/setup/puppet/modules/env/manifests/big.pp
@@ -17,7 +17,7 @@ class env::big ( $variant = "big", $parent_parameters = {} ){
   # kvm
   class { 'env::big::configure_kvm': }
   # nvidia
-  if $env::deb_arch == 'amd64' or $env::deb_arch == 'ppc64el' or $env::deb_arch == 'arm64' {
+  if $env::deb_arch == 'amd64' or $env::deb_arch == 'ppc64el' or ($env::deb_arch == 'arm64' and $::lsbdistcodename == 'trixie') {
     class { 'env::big::configure_nvidia_gpu': }
   }
   if $env::deb_arch == 'amd64' {
