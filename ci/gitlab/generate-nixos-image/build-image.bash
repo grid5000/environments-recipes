67e35bd46b706abe363311c2f431c4685a8bcce7
Ludovic Schoepps <ludovic.schoepps@uni.lu>
2026-06-12 13:18:32 +0200

    [nixos] Fix generated kadeploy description and tarball name

--- ci/gitlab/generate-nixos-image/build-image.bash
+++ ci/gitlab/generate-nixos-image/build-image.bash
@@ -2,4 +2,8 @@
 
 set -euo pipefail
 
+source .env
+sed -i "s/\$GENERATED_ENV_VERSION/${GENERATED_ENV_VERSION}/" configuration.nix
+sed -i "s/\$CI_PIPELINE_ID/${CI_PIPELINE_ID}/" configuration.nix
+sed -i "s/\$CI_COMMIT_SHORT_SHA/${CI_COMMIT_SHORT_SHA}/" configuration.nix
 nix --extra-experimental-features 'nix-command flakes' build .#g5k-image
--- ci/gitlab/generate-nixos-image/configuration.nix
+++ ci/gitlab/generate-nixos-image/configuration.nix
@@ -1,6 +1,56 @@
-{ pkgs, ... }:
+{ pkgs, lib, config, ... }:
+let
+  version = "$GENERATED_ENV_VERSION";
+  pipelineId = "$CI_PIPELINE_ID";
+  commitSha = "$CI_COMMIT_SHORT_SHA";
+in
 {
   environment.systemPackages = with pkgs; [ vim ];
 
   system.stateVersion = "26.05";
+
+  # Fix the generated kadeploy env description
+  system.build.kadeploy_env_description = lib.mkForce (pkgs.writeTextFile {
+    name = "nixos2605-x64-min.dsc";
+    text = ''
+      name: nixos2605-min
+      alias: nixos2605-x64-min
+      arch: x86_64
+      version: ${version}
+      description: NixOS 26.05 for x86_64 - min
+      author: support-staff@lists.grid5000.fr
+      visibility: public
+      destructive: false
+      os: linux
+      image:
+        file: http://public.nancy.grid5000.fr/~ajenkins/environments/pipelines/${pipelineId}-${commitSha}/nixos2605-x64-min.tar.xz
+        kind: tar
+        compression: xz
+      postinstalls:
+      - archive: server:///grid5000/postinstalls/g5k-postinstall.tgz
+        compression: gzip
+        script: g5k-postinstall --net none,predictable_kernel_name,traditional-names --bootloader no-grub-from-deployed-env
+      boot:
+        kernel: /boot/bzImage
+        initrd: /boot/initrd
+        kernel_params: init=boot/init console=tty0 console=ttyS0,115200
+      filesystem: ext4
+      partition_type: 131
+      multipart: false
+    '';
+  });
+
+  # Fix the name of the generated files
+  system.build.g5k-image = lib.mkForce (pkgs.stdenv.mkDerivation {
+    name = "g5k-image";
+    dontUnpack = true;
+    doCheck = false;
+
+    installPhase = ''
+      mkdir $out
+
+      ln -s ${config.system.build.kadeploy_env_description} $out/nixos2605-x64-min.dsc
+      ln -s ${config.system.build.g5k-image-archive}/tarball/*.tar.xz $out/nixos2605-x64-min.tar.xz
+    '';
+  });
 }
