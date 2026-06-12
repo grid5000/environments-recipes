6b8dae05f055f83d4ccf0b6617f8f859efe78c10
Ludovic Schoepps <ludovic.schoepps@uni.lu>
2026-06-12 11:44:29 +0200

    [nixos] Copy built env to home directory + reorganize bash files

---
+++ ci/gitlab/generate-nixos-image/flake.nix
@@ -0,0 +1,38 @@
+# Based on the minimal template from https://github.com/oar-team/nixos-g5k-image/tree/master/templates/minimal
+{
+  description = "Default NixOS image for Grid'5000 Testbed";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+    nixos-g5k-image = {
+      url = "github:oar-team/nixos-g5k-image";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      nixos-g5k-image,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+
+      nixosConfig = nixpkgs.lib.nixosSystem {
+        inherit system;
+
+        modules = [
+          nixos-g5k-image.nixosModules.g5k-image
+          ./configuration.nix
+        ];
+      };
+    in
+
+    {
+      packages.${system} = rec {
+        g5k-image = nixosConfig.config.system.build.g5k-image;
+        default = g5k-image;
+      };
+    };
+}
