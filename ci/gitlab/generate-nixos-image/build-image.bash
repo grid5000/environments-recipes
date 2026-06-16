596ec9d0bda28aba8e41cd9409f7e8209084022f
Ludovic Schoepps <ludovic.schoepps@uni.lu>
2026-06-16 11:46:25 +0200

    [nixos] Add required /etc/{grid5000/release,motd} files

--- ci/gitlab/generate-nixos-image/build-image.bash
+++ ci/gitlab/generate-nixos-image/build-image.bash
@@ -6,4 +6,6 @@ source .env
 sed -i "s/\$GENERATED_ENV_VERSION/${GENERATED_ENV_VERSION}/" configuration.nix
 sed -i "s/\$CI_PIPELINE_ID/${CI_PIPELINE_ID}/" configuration.nix
 sed -i "s/\$CI_COMMIT_SHORT_SHA/${CI_COMMIT_SHORT_SHA}/" configuration.nix
+sed -i "s/\$CI_COMMIT_SHA/${CI_COMMIT_SHA}/" configuration.nix
+
 nix --extra-experimental-features 'nix-command flakes' build .#g5k-image
