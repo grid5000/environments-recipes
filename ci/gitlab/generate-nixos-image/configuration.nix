72eb38d56ae8cc2942d8510457d207b41ff3e5d3
Ludovic Schoepps <ludovic.schoepps@uni.lu>
2026-07-10 16:51:30 +0200

    [nixos] Fix compatibility with generated grub.cfg from minios

--- ci/gitlab/generate-nixos-image/configuration.nix
+++ ci/gitlab/generate-nixos-image/configuration.nix
@@ -32,24 +32,70 @@ in {
 
   time.timeZone = "Europe/Paris";
   boot.loader = {
-    systemd-boot.enable = true;
-    # We need to change the boot order on rebuild otherwise we will keep using the initial config known by kadeploy
-    #efi = {
-    #  canTouchEfiVariables = true;
-    #  efiSysMountPoint = "/boot/efi";
-    #};
+    # Grub is required for kadeploy compatiblity and PXE boot
+    grub = {
+      enable = true;
+      device = "nodev";
+    };
+    efi = {
+      efiSysMountPoint = "/boot/efi";
+    };
   };
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-partlabel/KDPL_DEPLOY_disk0";
-    fsType = "ext4";
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-partlabel/KDPL_DEPLOY_disk0";
+      fsType = "ext4";
+    };
+    "/boot/efi" = {
+      device = "/dev/disk/by-partlabel/efi";
+      fsType = "vfat";
+      options = ["fmask=0022" "dmask=0022"];
+    };
   };
-  # We need to mount the EFI partition to be able to change the boot order on rebuild
-  #fileSystems."/boot/efi" = {
-  #  device = "/dev/disk/by-partlabel/efi";
-  #  fsType = "vfat";
-  #  options = ["fmask=0022" "dmask=0022"];
-  #};
+
+  # The first boot will use a simplified grub.cfg generated from the minios with the correct kernel parameters
+  # For subsequent boots, we need to copy these parameters before generating our own grub.cfg
+  systemd.services.save-kadeploy-cmdline = {
+    description = "Save Kadeploy kernel parameters for subsequent nixos-rebuilds";
+    wantedBy = ["multi-user.target"];
+
+    unitConfig = {
+      # Run only if the file hasn't been created yet
+      ConditionPathExists = "!/etc/nixos/kadeploy-cmdline.txt";
+    };
+
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+
+    script = ''
+      read -r cmdline < /proc/cmdline
+      filtered_params=()
+
+      for param in $cmdline; do
+        case "$param" in
+          # Filter out params managed by NixOS bootloader or kadeploy's deployment process
+          init=*|root=*|rw)
+            ;;
+          *)
+            filtered_params+=("$param")
+            ;;
+        esac
+      done
+
+      # Write the filtered parameters space-separated to the nixos folder
+      echo -n "''${filtered_params[*]}" > /etc/nixos/kadeploy-cmdline.txt
+    '';
+  };
+
+  boot.kernelParams = let
+    cmdlineFile = ./kadeploy-cmdline.txt;
+  in
+    if builtins.pathExists cmdlineFile
+    then builtins.filter (x: x != "") (lib.splitString " " (lib.trim (builtins.readFile cmdlineFile)))
+    else [];
 
   # TODO: using networkmanager currently breaks ipv6?
   networking.useDHCP = true;
